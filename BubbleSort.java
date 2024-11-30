d52c00f8516de06afbe142b3c8737240a261eb61
Ghgh23 <157329424+Ghgh23@users.noreply.github.com>
2024-11-30 22:19:48 +0900

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,40 @@
+public class BubbleSort {
+    public static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(array, j, j + 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    private static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Исходный массив:");
+        printArray(array);
+
+        bubbleSort(array);
+
+        System.out.println("Отсортированный массив:");
+        printArray(array);
+    }
+
+    private static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
